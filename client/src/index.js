9350b12c27d1f8b6827da76bf13e83574e851f13
Huanliang Fan <fanhuanliang@gmail.com>
2021-01-16 20:09:35 -0800

    Build up the database schema. Create three tables: users, products and reviewsList.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './component/App.jsx';
+import App from './component/App.jsx'; // eslint-disable-line
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+  <App />,// eslint-disable-line
+  document.getElementById('app')// eslint-disable-line
+)// eslint-disable-line
--- schema.sql
+++ schema.sql
@@ -3,19 +3,43 @@ CREATE DATABASE FEC;
 USE FEC;
 
 CREATE TABLE reviewList (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   date_create DATE NOT NULL,
   overall_rate INT NOT NULL,
-  username VARCHAR(500) NOT NULL,
-  age VARCHAR(200) NOT NULL,
+  username VARCHAR(2555) NOT NULL,
+  age INT(200) NOT NULL,
   recommend BOOLEAN,
-  purchase_for VARCHAR(500) NOT NULL,
-  title VARCHAR(200) NOT NULL,
+  purchase_for VARCHAR(255),
+  title VARCHAR(255) NOT NULL,
   content TEXT NOT NULL,
   play_experience INT,
   level_difficulty INT,
+  value_for_money INT,
   build_time INT,
-  building_experience VARCHAR(200),
-  helpful_yes: INT,
-  helpful_no: INT,
-)
+  building_experience VARCHAR(255),
+  helpful_yes INT,
+  helpful_no INT,
+  image1 VARCHAR(255),
+  image2 VARCHAR(255),
+  image3 VARCHAR(255),
+  image4 VARCHAR(255),
+  image5 VARCHAR(255),
+  product_id INT NOT NULL,
+  users_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (product_id) REFERENCES products(id),
+  FOREIGN KEY (users_id) REFERENCES users(id)
+);
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  item_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
