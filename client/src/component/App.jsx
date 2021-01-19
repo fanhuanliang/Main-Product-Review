73646fa67bd2eb5e8448a398792fdc8cd3086696
Huanliang Fan <fanhuanliang@gmail.com>
2021-01-18 17:58:58 -0800

    Implement the GET, connect client to the the server and the database.

--- client/src/component/App.jsx
+++ client/src/component/App.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  render () {
+  getRequest() {
+    axios.get('/api/products/1')
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }
+
+  componentDidMount() {
+    this.getRequest();
+  }
+
+  render() {
     return (
       <div>
         <h1>Review Test</h1>
